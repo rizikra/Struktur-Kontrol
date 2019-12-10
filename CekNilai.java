0a2206667d567f449caa71be498222a762c32653
Rizik <rizikardi01@gmail.com>
2019-12-10 13:13:04 +0700

    Belajar Struktur Kontrol pada java

---
+++ CekNilai.java
@@ -0,0 +1,30 @@
+public class CekNilai {
+	public static void main (String[] args){
+		
+		try {int input = Integer.parseInt(args[0]);
+		int nilai = input;
+		
+		if (nilai >= 85 &&  nilai <= 100){
+			System.out.println("A");
+		}
+		else if (nilai >= 75 && nilai <= 84){
+			System.out.println("B");
+		}
+		else if (nilai >= 65 && nilai <= 74){ 
+			System.out.println("C");
+		}
+		else if (nilai >= 45 && nilai <= 64){
+			System.out.println("D");
+		}
+		else if (nilai <= 44){
+			System.out.println("E");
+		}
+		else{
+			System.out.println("Nilai Tidak Valid, Silakan Cek Kembali !");	
+		}
+	}	
+		catch (Exception e){
+			System.out.println("Nilai Tidak Valid, Silakan Cek Kembali !");	
+		}
+	}
+}
---
+++ StrukturKontrolIf.java
@@ -0,0 +1,36 @@
+public class StrukturKontrolIf {
+	public static void main (String[] args){
+		//if (saja)	
+		boolean hujan = true;
+		if(hujan == true){	
+			System.out.println("Bawa payung");
+		}
+			System.out.println();
+		
+		//if-else
+		int nilai = 90;
+		if (nilai > 75){
+			System.out.println("Selamat, Anda LULUS!");
+		}
+		else{
+			System.out.println("Mohon Maaf, Anda TIDAK LULUS");
+		}
+			System.out.println();
+		
+		//if-else-if
+		boolean Hujan = false;
+		boolean adapayung = true;
+		if(Hujan == true && adapayung == true){	
+			System.out.println("Boleh keluar rumah");
+		}
+		else if (Hujan == true && adapayung == false){
+			System.out.println("Tidak boleh keluar rumah");
+		}
+		else if (Hujan == false && adapayung == true){
+			System.out.println("Boleh keluar rumah tapi payungnya tidak terpakai");
+		}
+		else{
+			System.out.println("Boleh keluar rumah meski payung tidak ada");
+		}
+	}	
+}
