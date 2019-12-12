de07b6b5f17f6d7b8bd106f375c051234416a968
Rizik <rizikardi01@gmail.com>
2019-12-12 09:55:49 +0700

    Revisi file StrukturKontrolIf.java

--- StrukturKontrolIf.java
+++ StrukturKontrolIf.java
@@ -7,32 +7,34 @@ public class StrukturKontrolIf {
 		}
 			System.out.println();
 		
-		//if-else
-		int nilai = 90;
-		if (nilai > 75){
-			System.out.println("Selamat, Anda LULUS!");
-		}
-		else{
-			System.out.println("Mohon Maaf, Anda TIDAK LULUS");
-		}
-			System.out.println();
+			
+			//if-else
+			int nilai = 90;
+			if (nilai > 75){
+				System.out.println("Selamat, Anda LULUS!");
+			}
+			else{
+				System.out.println("Mohon Maaf, Anda TIDAK LULUS");
+			}
+				System.out.println();
 		
-		//if-else-if
-		boolean Hujan = false;
-		boolean adapayung = true;
-		if(Hujan == true && adapayung == true){	
-			System.out.println("Boleh keluar rumah");
-		}
-		else if (Hujan == true && adapayung == false){
-			System.out.println("Tidak boleh keluar rumah");
-		}
-		else if (Hujan == false && adapayung == true){
-			System.out.println("Boleh keluar rumah tapi payungnya tidak terpakai");
-		}
-		else{
-			System.out.println("Boleh keluar rumah meski payung tidak ada");
-		}
-		System.out.println();
-		System.out.println();
+			
+			//if-else-if
+				boolean Hujan = false;
+				boolean adapayung = true;
+				if(Hujan == true && adapayung == true){	
+					System.out.println("Boleh keluar rumah");
+				}
+				else if (Hujan == true && adapayung == false){
+					System.out.println("Tidak boleh keluar rumah");
+				}
+				else if (Hujan == false && adapayung == true){
+					System.out.println("Boleh keluar rumah tapi payungnya tidak terpakai");
+				}
+				else{
+					System.out.println("Boleh keluar rumah meski payung tidak ada");
+				}
+					System.out.println();
+					System.out.println();
 	}	
 }
